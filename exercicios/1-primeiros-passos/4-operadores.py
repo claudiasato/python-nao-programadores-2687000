749d9500defd995398790556cef8567148ad6ec6
claudiasato <162748265+claudiasato@users.noreply.github.com>
2024-05-01 00:24:24 +0000

    execircio realizado

--- exercicios/1-primeiros-passos/4-operadores.py
+++ exercicios/1-primeiros-passos/4-operadores.py
@@ -2,9 +2,19 @@ ano_nascimento = 1989
 ano_formatura = 2010
 
 # Considerando que as variáveis acima correspondem a 'Gerlaine', descubra a idade dela no ano da sua formatura
-
+print(f"Gerlaine tinha {ano_formatura - ano_nascimento} anos quando se formou")
 
 # Escreva expressões comparativas usando os operadores relacionais >, <= e ==. Imprima na tela as respostas
+a = 2
+b = 3
+c = 4
+print(a == c)
+print(b > c)
+print(b <= a )
+
 
 
 # Crie expressões comparativas mais complexas utilizando operadores lógicos and, or e not. Imprima na tela as respostas
+print(a < b and c > b)
+print(a == b or c>= b)
+print(not a == b)
